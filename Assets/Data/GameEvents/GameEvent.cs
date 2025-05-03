2b467700febe334d679e69ef78bd5f23183724fa
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-05-03 07:50:43 +0200

    Added a basic global ScriptableObject event system

---
+++ Assets/Data/GameEvents/GameEvent.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "NewDialogueObject", menuName = "Data/Dialogue")]
+public class GameEvent : ScriptableObject
+{
+    // All objects that are observing this event
+    private readonly List<GameEventObserver> observers = new();
+
+    // Add new object to list of observers
+    public void AddObserver(GameEventObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    // Remove object from list of observers
+    public void RemoveObserver(GameEventObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    // Call this method when you want this GameEvent to be triggered
+    public void TriggerEvent()
+    {
+        // Debug log to see which and when a event is triggered.
+        // Debug.Log("GameEvent " + name + " Triggered");
+        // When this event is triggered, go through every observer it has and activate their response
+        foreach (GameEventObserver observer in observers)
+        {
+            observer.RespondToEvent(name);
+        }
+    }
+}
