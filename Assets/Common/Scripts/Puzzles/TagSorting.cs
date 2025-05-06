e8f283525c8088b35e84825523fe8c42dd23471d
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-05-06 15:43:33 +0200

    Made a simple colour sorting game using the puzzle controls
    The code is ugly, but I just needed something simple that worked.

---
+++ Assets/Common/Scripts/Puzzles/TagSorting.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TagSorting : MonoBehaviour
+{
+    [SerializeField] private List<SortingBucket> sortingBuckets = new();
+    public UnityEvent allBucketsSorted;
+
+
+    public void AddPointToBucket(int bucketIndex)
+    {
+        // Check to see if bucket exist
+        if (bucketIndex > sortingBuckets.Count - 1)
+        {
+            Debug.LogWarning($"Sorting bucket {bucketIndex} not found");
+            return;
+        }
+        SortingBucket bucket = sortingBuckets[bucketIndex];
+        bucket.currentPoints++;
+        Debug.Log($"Bucket {bucketIndex} has {bucket.currentPoints} points");
+        if (bucket.currentPoints >= bucket.requiredPoints)
+        {
+            bucket.done = true;
+            bucket.bucketDone.Invoke();
+            CheckIfBucketsAreDone();
+        }
+    }
+
+    public void RemovePointFromBucket(int bucketIndex)
+    {
+        // Check to see if bucket exist
+        if (bucketIndex > sortingBuckets.Count - 1)
+        {
+            Debug.LogWarning($"Sorting bucket {bucketIndex} not found");
+            return;
+        }
+        SortingBucket bucket = sortingBuckets[bucketIndex];
+        bucket.currentPoints--;
+        Debug.Log($"Bucket {bucketIndex} has {bucket.currentPoints} points");
+        if (bucket.currentPoints < bucket.requiredPoints)
+        {
+            bucket.done = false;
+            bucket.bucketUndone.Invoke();
+        }
+    }
+
+    private void CheckIfBucketsAreDone()
+    {
+        foreach (SortingBucket bucket in sortingBuckets)
+        {
+            if (!bucket.done)
+            {
+                return;
+            }
+        }
+        allBucketsSorted.Invoke();
+    }
+}
+
+[Serializable]
+public class SortingBucket
+{
+    public int requiredPoints;
+    public UnityEvent bucketDone;
+    public UnityEvent bucketUndone;
+    [HideInInspector] public int currentPoints = 0;
+    [HideInInspector] public bool done = false;
+}
