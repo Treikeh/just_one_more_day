6b625275c1debc7ab2d0d4617ac9ebf68e90c6cd
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-05-06 12:53:56 +0200

    Can see active and finished quests
    The system uses a public static variable which isn't the end of the world, but I would prefer to use a different method.

--- Assets/Common/Scripts/Managers/QuestManager.cs
+++ Assets/Common/Scripts/Managers/QuestManager.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class QuestManager : MonoBehaviour
 {
     private Dictionary<string, Quest> questMap;
-    private Dictionary<string, Quest> activeQuests = new();
-    private Dictionary<string, Quest> finishedQuests = new();
+    // !Having this as a public static variable is only temporary
+    public static Dictionary<string, Quest> activeQuests = new();
+    // !Having this as a public static variable is only temporary
+    public static Dictionary<string, Quest> finishedQuests = new();
 
     private void Awake()
     {
--- Assets/Interface/Journal/JournalUi.cs
+++ Assets/Interface/Journal/JournalUi.cs
@@ -1,8 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class JournalUi : MonoBehaviour
 {
     [SerializeField] private GameObject journalPanel;
+    [SerializeField] private TMP_Text activeQuests;
+    [SerializeField] private TMP_Text finishedQuests;
 
     private void OnEnable()
     {
@@ -18,7 +21,7 @@ public class JournalUi : MonoBehaviour
 
 
     // Hide Journal
-    public void CancelPressed()
+    private void CancelPressed()
     {
         if (journalPanel.activeInHierarchy)
         {
@@ -34,6 +37,32 @@ public class JournalUi : MonoBehaviour
         {
             journalPanel.SetActive(true);
             GameEventManager.Instance.inputEvents.ActionMapChanged("Ui");
+
+            // Display active quests
+            activeQuests.text = "Active Quessts:\n";
+            foreach (string key in QuestManager.activeQuests.Keys)
+            {
+                activeQuests.text += $"{QuestManager.activeQuests[key].info.description}: {QuestManager.activeQuests[key].questProgress}/{QuestManager.activeQuests[key].info.questSteps}\n";
+            }
+
+            // Display finished quests
+            finishedQuests.text = "Finished Quests:\n";
+            foreach (string key in QuestManager.finishedQuests.Keys)
+            {
+                finishedQuests.text += $"{QuestManager.finishedQuests[key].info.description}";
+            }
         }
+    }
+
+    // UI BUTTON EVENTS
+    public void OnResumePressed()
+    {
+        // Hide jounral
+        CancelPressed();
+    }
+
+    public void OnQuitPressed()
+    {
+        Application.Quit();
     }
 }
