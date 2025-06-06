8ef91471ed0c1530c6e72e427eeec464dff22e52
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-06-06 12:41:03 +0200

    Added new buttons

--- Assets/Interface/MainMenu/MainMenu.cs
+++ Assets/Interface/MainMenu/MainMenu.cs
@@ -25,4 +25,9 @@ public class MainMenu : MonoBehaviour
     {
         SaveManager.LoadGame();
     }
+
+    public void OnCreditsButtonPressed()
+    {
+        LevelManager.Instance.StartLoadingLevel("Credits");
+    }
 }
