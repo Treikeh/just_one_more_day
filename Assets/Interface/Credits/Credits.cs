2e58349f99082c5922e45a9ce082fe3facee3742
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-06-09 17:07:51 +0200

    Changed colliders on a lot of props + Other smaller changes

--- Assets/Interface/Credits/Credits.cs
+++ Assets/Interface/Credits/Credits.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
+
+    private void Start()
+    {
+        QuestManager.Instance.hasJournal = false;
+    }
+
+
     public void OnMainMenuButtonPressed()
     {
         LevelManager.Instance.StartLoadingLevel("MainMenu");
